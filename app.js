a2defef68411c1582d096a0c9e53e9cd683f816a
Tomasz Szymański <tpp.szymanski@gmail.com>
2022-04-19 12:00:45 +0200

    wersja 1.1

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 const express = require('express')
 const port = 3000
+=======
+//wersja 1.1
+const express = require('express')
+const port = parseInt(process.env.PORT) || 3000
+>>>>>>> 5b62e6b (wersja 1.1)
 
 const app = express()
 app.set('view engine', 'hbs')
