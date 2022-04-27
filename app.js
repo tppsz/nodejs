4534af4e70ae7ceb3351af301bf951485c85f7b8
Tomasz Szymański <tpp.szymanski@gmail.com>
2022-04-27 11:23:36 +0200

    database connection

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 //wersja 1.1
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "elearning2"
+
+});
+
+con.connect(function(err){
+    if (err) throw err;
+    con.query("SELECT name FROM users", function(err, result, fields){
+        if (err) throw err;
+        console.log(result); 
+    });
+});
+
 const express = require('express')
 const port = parseInt(process.env.PORT) || 3000
 
@@ -8,7 +26,7 @@ app.set('view engine', 'hbs')
 app.get('/', (req, res) => {
     res.render('index', {
         pageTitle: 'node.js',
-        pageBody: 'Body'
+        pageBody: response
     })
 })
 
